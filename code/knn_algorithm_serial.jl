b9d60ead6b178114795d62e3bb55688ae167f67e
Rushikesh Gaidhani <rushi.rg@gmail.com>
2017-05-13 00:27:05 +0530

    Updated algorithms

---
+++ code/knn_algorithm_serial.jl
@@ -0,0 +1,128 @@
+
+using Images
+using DataFrames
+
+#typeData could be either "train" or "test.
+#labelsInfo should contain the IDs of each image to be read
+#The images are 20x20 pixels, so imageSize is set to 400.
+#path should be set to the location of the data files.
+counter = 0 #counter
+
+function read_data(typeData, labelsInfo, imageSize, path)
+ #Intialize x matrix
+ x = zeros(size(labelsInfo, 1), imageSize)
+
+ for (index, idImage) in enumerate(labelsInfo[1]) 
+  #Read image file 
+  nameFile = "$(path)/$(typeData)/$(idImage).Bmp"
+  img = load(nameFile)
+
+  temp = convert(Image{Gray}, img)
+
+  global counter = counter + 1
+
+  println("Reading $(typeData) image ... $(counter).Bmp ") #printing counter
+
+
+  #Transform image matrix to a vector and store 
+	  #it in data matrix 
+  x[index, :] = reshape(temp, 1, imageSize)
+ end 
+ return x
+end
+
+imageSize = 400 # 20 x 20 pixel
+
+#Set location of data files, folders
+path = "/home/rushi/Desktop/old_Desktop/BTech_Project/Project/data"
+
+#Read information about training data , IDs.
+#@@@my-comment@@@# labelsInfoTrain = readtable("$(path)/trainLabels.csv")
+
+#Read training matrix
+xTrain = load("/home/rushi/Desktop/old_Desktop/BTech_Project/Project/data/code/models/RandomForestXTrain.jld", "xTrain")
+
+#Read information about test data ( IDs ).
+labelsInfoTest = readtable("$(path)/code/test_labels/testLabels1.csv")
+
+#Read test matrix
+xTest = read_data("test1", labelsInfoTest, imageSize, path)
+
+#Get only first character of string (convert from string to character).
+#Apply the function to each element of the column "Class"
+#@@@@my-comment@@@@# yTrain = map(x -> x[1], labelsInfoTrain["Class"])
+
+#Convert from character to integer
+#@@@my-commenet@@@@# yTrain = int(yTrain)
+yTrain = load("/home/rushi/Desktop/old_Desktop/BTech_Project/Project/data/code/models/RandomForestYTrain.jld", "yTrain")
+
+
+xTrain = xTrain'
+xTest = xTest'
+
+
+function euclidean_distance(a, b)
+ distance = 0.0 
+ for index in 1:size(a, 1) 
+  distance += (a[index]-b[index]) * (a[index]-b[index])
+ end
+ return distance
+end
+
+function get_k_nearest_neighbors(xTrain, imageI, k)
+ nRows, nCols = size(	xTrain) 
+ imageJ = Array(Float32, nRows)
+ distances = Array(Float32, nCols) 
+ for j in 1:nCols
+  for index in 1:nRows
+   imageJ[index] = xTrain[index, j]
+  end
+  distances[j] = euclidean_distance(imageI, imageJ)
+ end
+ sortedNeighbors = sortperm(distances)
+ kNearestNeighbors = sortedNeighbors[1:k]
+ return kNearestNeighbors
+end
+
+function assign_label(xTrain, yTrain, k, imageI)
+ kNearestNeighbors = get_k_nearest_neighbors(xTrain, imageI, k) 
+ counts = Dict{Int, Int}() 
+ highestCount = 0
+ mostPopularLabel = 0
+ for n in kNearestNeighbors
+  labelOfN = yTrain[n]
+  if !haskey(counts, labelOfN)
+   counts[labelOfN] = 0
+  end
+  counts[labelOfN] += 1 #add one to the count
+  if counts[labelOfN] > highestCount
+   highestCount = counts[labelOfN]
+   mostPopularLabel = labelOfN
+  end 
+ end
+ return mostPopularLabel
+end
+
+println("Running kNN on test data")
+tic()
+k = 3 # The CV accuracy shows this value to be the best.
+yPredictions =72 
+for i in 2:size(xTest, 2)
+ nRows = size(xTrain, 1)
+ imageI = Array(Float32, nRows)
+ for index in 1:nRows
+  imageI[index] = xTest[index, i]
+ end
+ yPredictions = vcat(yPredictions, assign_label(xTrain, yTrain, k, imageI))
+end
+toc()
+
+
+#Convert integer predictions to character
+labelsInfoTest[2] = map(Char, yPredictions)
+
+
+#Save predictions
+writetable("$(path)/code/Output/juliaKNNSerialSubmission.csv", labelsInfoTest, separator=',', header=true)
+
+println("Submission file saved in $(path)/code/Output/juliaKNNSerialSubmission.csv")	
---
+++ code/random_forest_with_stored_model.jl
@@ -0,0 +1,84 @@
+using Images
+using DataFrames
+using DecisionTree
+using JLD #using JLD to store model once trained
+
+#typeData could be either "train" or "test.
+#labelsInfo should contain the IDs of each image to be read
+#20x20 pixels images, so imageSize is set to 400.
+#path set to the location of the data files.
+
+counter = 0 #counter
+
+function read_data(typeData, labelsInfo, imageSize, path)
+ #Intialize x matrix
+ x = zeros(size(labelsInfo, 1), imageSize)
+
+ for (index, idImage) in enumerate(labelsInfo[1]) 
+  #Read image file 
+  nameFile = "$(path)/$(typeData)/$(idImage).Bmp"
+  img = load(nameFile)
+
+  temp = convert(Image{Gray}, img)
+
+ global counter = counter + 1
+
+  println("Reading $(typeData) image ... $(counter).Bmp ") #printing counter
+    
+  #Transform image matrix to a vector and store 
+  #it in data matrix 
+  x[index, :] = reshape(temp, 1, imageSize)
+ end 
+ return x
+end
+
+addprocs(4)
+import DecisionTree 			#importing package on each worker
+@everywhere using DecisionTree 	#loading package
+
+imageSize = 400 # 20 x 20 pixel
+
+#Set location of data files, folders
+path = "/home/rushi/Desktop/old_Desktop/BTech_Project/Project/data"
+
+#Read information about training data , IDs.
+#my-comment# labelsInfoTrain = readtable("$(path)/trainLabels.csv")
+
+#Read training matrix
+#my-comment# xTrain = read_data("train", labelsInfoTrain, imageSize, path)
+xTrain = load("/home/rushi/Desktop/old_Desktop/BTech_Project/Project/data/code/models/RandomForestXTrain.jld", "xTrain")
+
+
+#Read information about test data ( IDs ).
+labelsInfoTest = readtable("$(path)/code/test_labels/testLabels1.csv")
+
+#Read test matrix
+xTest = read_data("test1", labelsInfoTest, imageSize, path)
+
+#Get only first character of string (convert from string to character).
+#Apply the function to each element of the column "Class"
+#my-comment# yTrain = map(x -> x[1], labelsInfoTrain[2])
+
+#my-comment# yTrain = convert(Array{Int64,1},yTrain)
+
+tic()
+#Convert from character to integer
+#my-comment# yTrain = map(Int,yTrain)
+yTrain = load("/home/rushi/Desktop/old_Desktop/BTech_Project/Project/data/code/models/RandomForestYTrain.jld", "yTrain")
+
+model = build_forest(yTrain, xTrain, 20, 100, 1.0, 10)
+
+predTest = apply_forest(model, xTest)
+
+#Convert integer predictions to character
+labelsInfoTest[2] = map(Char,predTest)
+toc()
+
+#Save predictions
+writetable("juliaSubmissionRF_Model.csv", labelsInfoTest, separator=',', header=true)
+
+accuracy = nfoldCV_forest(yTrain, xTrain, 0, 100, 5, 1.0);
+println("5 fold accuracy: $(mean(accuracy))")	
+
+println("Submission file saved in $(path)/code/juliaSubmissionRF_Model.csv")
+
